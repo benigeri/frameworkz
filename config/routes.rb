16bdd58b931a93c2cc467a19de27488e5bc09a04
Paul Benigeri <me@benigeri.com>
2012-09-10 01:02:35 -0700

    start implementation of devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Frameworkz::Application.routes.draw do
+
+  resources :pages
+
+  root :to => "pages#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
