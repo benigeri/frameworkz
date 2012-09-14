8dd0980861d5eac88e71e850afe1cc281fbe97f2
Paul Benigeri <me@benigeri.com>
2012-09-13 22:06:45 -0700

    pre push to heroku

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,7 +4,8 @@ class VotesController < ApplicationController
 
   # user creates a comment, then instead of going to another, redirects to view just takes him back to the page where he added the comment
   def create
-    @vote = current_user.votes.create(params[:vote])
+    @vote = current_user.votes.where(:link_id => params[:vote][:link_id]).first || current_user.votes.create(params[:vote])
+    @vote.update_attributes(:up => params[:vote][:up])
     redirect_to :back
   end
 
