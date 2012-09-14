8fa7e881f40a08d0c31ddb5bb377b9041c2a6cb9
Paul Benigeri <me@benigeri.com>
2012-09-13 23:24:04 -0700

    sort links by vote count

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     params[:per_page] ||= 5
     params[:page]     ||= 1
 
-    @links = Link.order('created_at DESC').paginate(:page => params[:page], :per_page => params[:per_page])
+    @links = Link.order('voteCount DESC').paginate(:page => params[:page], :per_page => params[:per_page])
 
   end
 end
