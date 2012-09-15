03ae5ac284b7dea121b524958b167770dc9fe2b9
Paul Benigeri <me@benigeri.com>
2012-09-14 20:15:32 -0700

    migrated to postgres, finally all works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120914051534) do
+ActiveRecord::Schema.define(:version => 20120915023528) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(:version => 20120914051534) do
     t.boolean  "up"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "wineries", :force => true do |t|
+    t.string   "name"
+    t.string   "street_address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "website"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
 end
